b50d83f7e4cc49d1b0c30f0e325a504f7b5deb3d
Nate Douglas <ndouglas@users.noreply.github.com>
2026-09-21 19:00:36 -0400

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        match n:
+            case 0:
+                return 0
+            case 1:
+                return 1
+            case 2:
+                return 2
+            case _:
+                memo = [0, 1]
+                for i in range(2, n + 2):
+                    [a, b] = memo[0], memo[1]
+                    memo[0] = b
+                    memo[1] = a + b
+                return memo[1]
